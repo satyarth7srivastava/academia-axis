9aa49550d2de1bed455260dea752051e22dd3059
Satya <satyarths.ug23.ec@nitp.ac.in>
2024-08-11 10:55:07 +0530

    HomePage Updated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,25 +8,30 @@ export default function HomePage() {
     return (
         <div>
             <NavBar />
-            <h1 
-            className="text-4xl text-center mt-8"
-            >Best Free Resources</h1>
-            <h2
-            className="text-2xl text-center mt-4"
-            >Your One-Stop Solution for Learning</h2>
-            <Link href="/search"
-            className="text-center mt-4 block text-blue-500 border border-blue-500 rounded-lg p-2 w-1/4 mx-auto on-hover-bg-blue-300 on-hover-text-white"
+            {/* try to add some min-height so that ut would look good */}
+            <div 
+            className=""
             >
-               Explore Courses
-            </Link>
-            <Link href="/login"
-            className="text-center mt-4 block text-blue-500 border border-blue-500 rounded-lg p-2 w-1/4 mx-auto on-hover-bg-blue-300 on-hover-text-white"
-            >
-                Login To Continue learning
-            </Link>
-            <p
-            className="text-lg text-center mt-4"
-            >Access various free courses to enhance your knowledge and skills.</p>
+                <h1
+                    className="text-4xl text-center mt-8"
+                >Best Free Resources</h1>
+                <h2
+                    className="text-2xl text-center mt-4"
+                >Your One-Stop Solution for Learning</h2>
+                <Link href="/search"
+                    className="text-center mt-4 block text-blue-500 border border-blue-500 rounded-lg p-2 w-1/4 mx-auto on-hover-bg-blue-300 on-hover-text-white"
+                >
+                    Explore Courses
+                </Link>
+                <Link href="/login"
+                    className="text-center mt-4 block text-blue-500 border border-blue-500 rounded-lg p-2 w-1/4 mx-auto on-hover-bg-blue-300 on-hover-text-white"
+                >
+                    Login To Continue learning
+                </Link>
+                <p
+                    className="text-lg text-center mt-4"
+                >Access various free courses to enhance your knowledge and skills.</p>
+            </div>
             <Footer />
         </div>
     );
